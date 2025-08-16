668a006af769bdcb052dc0471508b1e08bafc257
Filip Navara <filip.navara@gmail.com>
2025-08-16 22:00:36 +0200

    Disable SetCanResize call that adds unintended window styles

--- libcef/browser/native/window_delegate_view.cc
+++ libcef/browser/native/window_delegate_view.cc
@@ -31,7 +31,7 @@ void CefWindowDelegateView::Init(gfx::AcceleratedWidget parent_widget,
   web_view_->SetWebContents(web_contents);
   web_view_->SetPreferredSize(bounds.size());
 
-  SetCanResize(true);
+  // SetCanResize(true);
 
   views::Widget* widget = new views::Widget;
 
